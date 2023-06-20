f6e1246dc4b09d8bd29f35b691247de194f91fb6
IlliaPopov1791 <illia.m.popov@gmail.com>
2023-06-19 20:13:02 -0400

    First Commit: the initial build of the program.

---
+++ IlliaModule/src/main/java/illia/popov/lab5/PopovActivity.java
@@ -0,0 +1,14 @@
+package illia.popov.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopovActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
